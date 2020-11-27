23af7de3e969d89ffed8299938e48c3d19d8fb2e
Eray Bakır <info@eraybakir.com>
2020-11-27 16:12:06 +0300

    Rol listeleme oluşturuldu

---
+++ IdentityTraining/Controllers/RoleController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityTraining.Context;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityTraining.Controllers
+{
+    [Authorize] //Giriş yapılmış olmalı 
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<AppRole> _roleManager;//Tıpkı user manager gibi role manager da role ile ilgili işlemleri yapar
+
+        public RoleController(RoleManager<AppRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        public IActionResult Index()
+        {
+            var roles = _roleManager.Roles.ToList();//Database de ki rollerin hepsini aldık
+            return View(roles);
+        }
+    }
+}
