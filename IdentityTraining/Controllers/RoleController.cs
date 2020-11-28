c7bf617e1e8927a3b1b0c95efdcec6111e279df2
Eray Bakır <info@eraybakir.com>
2020-11-28 12:46:13 +0300

    Rol Ekleme sayfası oluşturuldu

--- IdentityTraining/Controllers/RoleController.cs
+++ IdentityTraining/Controllers/RoleController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using IdentityTraining.Context;
+using IdentityTraining.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -23,6 +24,32 @@ namespace IdentityTraining.Controllers
         {
             var roles = _roleManager.Roles.ToList();//Database de ki rollerin hepsini aldık
             return View(roles);
+        }
+
+        public IActionResult AddRole()
+        {
+            return View(new RoleViewModel());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddRole(RoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var appRole = new AppRole() {Name=model.Name }; //App Role Entity si oluşturduk
+                var result = await _roleManager.CreateAsync(appRole); //Rolü oluşturuluyoruz
+
+                if (result.Succeeded) //Başarılıysa
+                {
+                    return RedirectToAction("Index");
+                }
+
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("",error.Description); //Hataları modele ekleyip gönderdik
+                }
+            }
+            return View(model);
         }
     }
 }
---
+++ IdentityTraining/Models/RoleViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityTraining.Models
+{
+    public class RoleViewModel //Rol ekleme modelimiz hazır
+    {
+        [Required(ErrorMessage ="Ad Alanı gereklidir")]
+        [Display(Name="Ad")]
+        public string Name { get; set; }
+    }
+}
